0c0c1c230ca3bef5cd1a6fe0d72c962149b7d625
Ibukunola <bulun1001@gmail.com>
2024-12-19 12:43:37 +0100

    feat(config): add tailwind font config

--- tailwind_SCSS/tailwind/tailwind.config.js
+++ tailwind_SCSS/tailwind/tailwind.config.js
@@ -2,10 +2,25 @@
 module.exports = {
   content: ["../*.html"],
   theme: {
+
+
     extend: {
+      // Fonts
+      fontFamily: {
+        playwrite: ['Playwrite ES Deco Guides', 'serif'],
+        playwriteVN: ['Playwrite VN Guides', 'serif'],
+      },
+      // End of Fonts
+
+      
+
+      // Spacing
       spacing: {
         '128': '400px',
       }
+      // End of Spacing
+
+
     },
   },
   plugins: [],
