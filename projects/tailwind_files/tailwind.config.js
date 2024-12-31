1270e7421f057b299941c32ee8e17ecf896c945d
Ibukunola <bulun1001@gmail.com>
2024-12-31 14:01:43 +0100

    feat: add the banner and header

--- projects/tailwind_files/tailwind.config.js
+++ projects/tailwind_files/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "../**/*.html"
+    "../**/*.html",
+    "../**/pages/*.html"
   ],
   theme: {
     extend: {
@@ -27,4 +28,5 @@ module.exports = {
   },
   plugins: [],
 }
+
 
